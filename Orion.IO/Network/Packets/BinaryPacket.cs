020e7b7b3aa87a73dab114c72065825f2af43879
Robert Lodico <lodico.rj@gmail.com>
2017-03-21 14:00:13 -0400

    Update BinaryPacket.cs

--- Orion.IO/Network/Packets/BinaryPacket.cs
+++ Orion.IO/Network/Packets/BinaryPacket.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
 MIT License
 
 Copyright (c) 2017 Robert Lodico
@@ -65,244 +65,244 @@ namespace Orion.IO.Network.Packets
             return true;
         }
 
-        public bool Read(ref bool value, long position = -1)
+        public bool Read(ref bool value)
         {
-            return Buffer.Read(ref value, position);
+            return Buffer.Read(ref value);
         }
 
-        public byte Read(ref byte value, long position = -1)
+        public byte Read(ref byte value)
         {
-            return Buffer.Read(ref value, position);
+            return Buffer.Read(ref value);
         }
 
-        public byte[] Read(ref byte[] value, long position = -1)
+        public byte[] Read(ref byte[] value)
         {
-            return Buffer.Read(ref value, position);
+            return Buffer.Read(ref value);
         }
 
-        public byte[] Read(ref byte[] value, int length, long position = -1)
+        public byte[] Read(ref byte[] value, int length)
         {
-            return Buffer.Read(ref value, length, position);
+            return Buffer.Read(ref value, length);
         }
 
-        public char Read(ref char value, long position = -1)
+        public char Read(ref char value)
         {
-            return Buffer.Read(ref value, position);
+            return Buffer.Read(ref value);
         }
 
-        public decimal Read(ref decimal value, long position = -1)
+        public decimal Read(ref decimal value)
         {
-            return Buffer.Read(ref value, position);
+            return Buffer.Read(ref value);
         }
 
-        public double Read(ref double value, long position = -1)
+        public double Read(ref double value)
         {
-            return Buffer.Read(ref value, position);
+            return Buffer.Read(ref value);
         }
 
-        public float Read(ref float value, long position = -1)
+        public float Read(ref float value)
         {
-            return Buffer.Read(ref value, position);
+            return Buffer.Read(ref value);
         }
 
-        public int Read(ref int value, long position = -1)
+        public int Read(ref int value)
         {
-            return Buffer.Read(ref value, position);
+            return Buffer.Read(ref value);
         }
 
-        public long Read(ref long value, long position = -1)
+        public long Read(ref long value)
         {
-            return Buffer.Read(ref value, position);
+            return Buffer.Read(ref value);
         }
 
-        public sbyte Read(ref sbyte value, long position = -1)
+        public sbyte Read(ref sbyte value)
         {
-            return Buffer.Read(ref value, position);
+            return Buffer.Read(ref value);
         }
 
-        public short Read(ref short value, long position = -1)
+        public short Read(ref short value)
         {
-            return Buffer.Read(ref value, position);
+            return Buffer.Read(ref value);
         }
 
-        public uint Read(ref uint value, long position = -1)
+        public uint Read(ref uint value)
         {
-            return Buffer.Read(ref value, position);
+            return Buffer.Read(ref value);
         }
 
-        public ulong Read(ref ulong value, long position = -1)
+        public ulong Read(ref ulong value)
         {
-            return Buffer.Read(ref value, position);
+            return Buffer.Read(ref value);
         }
 
-        public ushort Read(ref ushort value, long position = -1)
+        public ushort Read(ref ushort value)
         {
-            return Buffer.Read(ref value, position);
+            return Buffer.Read(ref value);
         }
 
-        public string Read(ref string value, long position = -1)
+        public string Read(ref string value)
         {
-            return Buffer.Read(ref value, position);
+            return Buffer.Read(ref value);
         }
 
-        public bool ReadBool(long position = -1)
+        public bool ReadBool()
         {
-            return Buffer.ReadBool(position);
+            return Buffer.ReadBool();
         }
 
-        public byte ReadByte(long position = -1)
+        public byte ReadByte()
         {
-            return Buffer.ReadByte(position);
+            return Buffer.ReadByte();
         }
 
-        public byte[] ReadBytes(long position = -1)
+        public byte[] ReadBytes()
         {
-            return Buffer.ReadBytes(position);
+            return Buffer.ReadBytes();
         }
 
-        public byte[] ReadBytes(int length, long position = -1)
+        public byte[] ReadBytes(int length)
         {
-            return Buffer.ReadBytes(length, position);
+            return Buffer.ReadBytes(length);
         }
 
-        public char ReadChar(long position = -1)
+        public char ReadChar()
         {
-            return Buffer.ReadChar(position);
+            return Buffer.ReadChar();
         }
 
-        public decimal ReadDecimal(long position = -1)
+        public decimal ReadDecimal()
         {
-            return Buffer.ReadDecimal(position);
+            return Buffer.ReadDecimal();
         }
 
-        public double ReadDouble(long position = -1)
+        public double ReadDouble()
         {
-            return Buffer.ReadDouble(position);
+            return Buffer.ReadDouble();
         }
 
-        public float ReadFloat(long position = -1)
+        public float ReadFloat()
         {
-            return Buffer.ReadFloat(position);
+            return Buffer.ReadFloat();
         }
 
-        public int ReadInt(long position = -1)
+        public int ReadInt()
         {
-            return Buffer.ReadInt(position);
+            return Buffer.ReadInt();
         }
 
-        public long ReadLong(long position = -1)
+        public long ReadLong()
         {
-            return Buffer.ReadLong(position);
+            return Buffer.ReadLong();
         }
 
-        public sbyte ReadSByte(long position = -1)
+        public sbyte ReadSByte()
         {
-            return Buffer.ReadSByte(position);
+            return Buffer.ReadSByte();
         }
 
-        public short ReadShort(long position = -1)
+        public short ReadShort()
         {
-            return Buffer.ReadShort(position);
+            return Buffer.ReadShort();
         }
 
-        public string ReadString(long position = -1)
+        public string ReadString()
         {
-            return Buffer.ReadString(position);
+            return Buffer.ReadString();
         }
 
-        public uint ReadUInt(long position = -1)
+        public uint ReadUInt()
         {
-            return Buffer.ReadUInt(position);
+            return Buffer.ReadUInt();
         }
 
-        public ulong ReadULong(long position = -1)
+        public ulong ReadULong()
         {
-            return Buffer.ReadULong(position);
+            return Buffer.ReadULong();
         }
 
-        public ushort ReadUShort(long position = -1)
+        public ushort ReadUShort()
         {
-            return Buffer.ReadUShort(position);
+            return Buffer.ReadUShort();
         }
 
-        public void Write(bool value, long position = -1)
+        public void Write(bool value)
         {
-            Buffer.Write(value, position);
+            Buffer.Write(value);
         }
 
-        public void Write(byte value, long position = -1)
+        public void Write(byte value)
         {
-            Buffer.Write(value, position);
+            Buffer.Write(value);
         }
 
-        public void Write(byte[] value, long position = -1)
+        public void Write(byte[] value)
         {
-            Buffer.Write(value, position);
+            Buffer.Write(value);
         }
 
-        public void Write(byte[] value, int length, long position = -1)
+        public void Write(byte[] value, int length)
         {
-            Buffer.Write(value, length, position);
+            Buffer.Write(value, length);
         }
 
-        public void Write(char value, long position = -1)
+        public void Write(char value)
         {
-            Buffer.Write(value, position);
+            Buffer.Write(value);
         }
 
-        public void Write(decimal value, long position = -1)
+        public void Write(decimal value)
         {
-            Buffer.Write(value, position);
+            Buffer.Write(value);
         }
 
-        public void Write(double value, long position = -1)
+        public void Write(double value)
         {
-            Buffer.Write(value, position);
+            Buffer.Write(value);
         }
 
-        public void Write(float value, long position = -1)
+        public void Write(float value)
         {
-            Buffer.Write(value, position);
+            Buffer.Write(value);
         }
 
-        public void Write(int value, long position = -1)
+        public void Write(int value)
         {
-            Buffer.Write(value, position);
+            Buffer.Write(value);
         }
 
-        public void Write(long value, long position = -1)
+        public void Write(long value)
         {
-            Buffer.Write(value, position);
+            Buffer.Write(value);
         }
 
-        public void Write(sbyte value, long position = -1)
+        public void Write(sbyte value)
         {
-            Buffer.Write(value, position);
+            Buffer.Write(value);
         }
 
-        public void Write(short value, long position = -1)
+        public void Write(short value)
         {
-            Buffer.Write(value, position);
+            Buffer.Write(value);
         }
 
-        public void Write(uint value, long position = -1)
+        public void Write(uint value)
         {
-            Buffer.Write(value, position);
+            Buffer.Write(value);
         }
 
-        public void Write(ulong value, long position = -1)
+        public void Write(ulong value)
         {
-            Buffer.Write(value, position);
+            Buffer.Write(value);
         }
 
-        public void Write(ushort value, long position = -1)
+        public void Write(ushort value)
         {
-            Buffer.Write(value, position);
+            Buffer.Write(value);
         }
 
-        public void Write(string value, long position = -1)
+        public void Write(string value)
         {
-            Buffer.Write(value, position);
+            Buffer.Write(value);
         }
     }
 }
